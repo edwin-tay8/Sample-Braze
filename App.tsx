16c6437eeb118a1f24218e25d751d019abc6d345
Edwin Tay <edwintay@propertyguru.com.sg>
2023-12-11 21:58:59 +0800

    no message

--- App.tsx
+++ App.tsx
@@ -6,7 +6,6 @@
  */
 
 import React, {useCallback, useEffect, useState} from 'react';
-import type {PropsWithChildren} from 'react';
 import {
   Alert,
   NativeModules,
@@ -20,44 +19,8 @@ import {
 } from 'react-native';
 import Braze from '@braze/react-native-sdk';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
 
 const ASYNC_STORAGE_KEY = 'notification-permission';
 
@@ -140,20 +103,7 @@ function App(): JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          <Text style={styles.highlight}>RN Braze SDK Version 8.2.0</Text>
         </View>
       </ScrollView>
     </SafeAreaView>
